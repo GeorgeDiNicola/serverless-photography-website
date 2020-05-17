6dded56535ad6906d26408f7a1ba627158b5f8a6
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-05-17 15:08:46 -0400

    completion of dynamic rendering youtube reels and formatting

--- static/src/components/Videography.jsx
+++ static/src/components/Videography.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import ReactPlayer from "react-player"
+import "../css/video.css"
 
 export default class Videography extends Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			video_urls: ["https://www.youtube.com/watch?v=DRaWc9GhYRk", "https://www.youtube.com/watch?v=lABN5Gld4FQ"]
+		}
+	}
+
 	render() {
 		return (
-		  <h1>test Videography page</h1>
+		  <React.Fragment>
+		  <h1>Professional Video Reels</h1>
+		  <div className="video-page">
+		    {this.state.video_urls.map(url_text => 
+		    <div className='player-wrapper'>
+		    <ReactPlayer
+		      className='react-player'
+		      url={url_text}
+		      width='100%'
+		      height='100%'
+		      controls={true}
+		    />
+		    </div>
+		    )}
+		  </div>
+		  </React.Fragment>
 		)
 	}
 }
