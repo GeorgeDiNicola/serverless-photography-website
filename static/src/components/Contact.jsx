d9c32e0f467f43ee8b74df37276e5079b7a0fd68
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-05-17 17:38:45 -0400

    development of contact page and added 60 more images

--- static/src/components/Contact.jsx
+++ static/src/components/Contact.jsx
@@ -1,10 +1,99 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import {
+  MDBCollapse,
+  MDBMask,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+  MDBBtn,
+  MDBView,
+  MDBContainer,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBFormInline,
+  MDBAnimation
+} from "mdbreact";
+import '../images/83.jpg';
+import '../css/contactme.css';
 
 export default class Contact extends Component {
 
 	render() {
-		return (
-		  <h1>test Contact page</h1>
-		)
-	}
+
+	  return (
+	   <div id="classicformpage">
+        <MDBView>
+          <MDBMask className="d-flex justify-content-center align-items-center gradient">
+            <MDBContainer>
+              <MDBRow>
+                <MDBAnimation
+                  type="fadeInLeft"
+                  delay=".3s"
+                  className="white-text text-center text-md-left col-md-6 mt-xl-5 mb-5"
+                >
+                  <h1 className="h1-responsive font-weight-bold">
+                    About Me
+                  </h1>
+                  <hr className="hr-light" />
+                  <h6 className="mb-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Rem repellendus quasi fuga nesciunt dolorum nulla magnam
+                    veniam sapiente, fugiat! Commodi sequi non animi ea dolor
+                    molestiae, quisquam iste, maiores. Nulla.
+                  </h6>
+                </MDBAnimation>
+
+                <MDBCol md="6" xl="5" className="mb-4">
+                  <MDBAnimation type="fadeInRight" delay=".3s">
+                    <MDBCard id="classic-card">
+                      <MDBCardBody className="white-text">
+                        <h2>Contact Me</h2>
+                        <hr className="hr-light" />
+                          <span class="glyphicon glyphicon-earphone" aria-hidden="true">
+                           (484)-574-2994</span>
+                          <span class="glyphicon glyphicon-envelope" aria-hidden="true"> 
+                            williamdenham107@gmail.com</span>
+                           <h5></h5>
+                           <h5></h5>
+                           <h5>LinkedIn</h5>
+                           <h5>Facebook</h5>
+                        <div className="text-center mt-4 black-text">
+                          <hr className="hr-light" />
+                          <div className="text-center d-flex justify-content-center white-label">
+                            <a href="#!" className="p-2 m-2">
+                              <MDBIcon
+                                fab
+                                icon="twitter"
+                                className="white-text"
+                              />
+                            </a>
+                            <a href="#!" className="p-2 m-2">
+                              <MDBIcon
+                                fab
+                                icon="linkedin"
+                                className="white-text"
+                              />
+                            </a>
+                            <a href="#!" className="p-2 m-2">
+                              <MDBIcon
+                                fab
+                                icon="instagram"
+                                className="white-text"
+                              />
+                            </a>
+                          </div>
+                        </div>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </MDBAnimation>
+                </MDBCol>
+              </MDBRow>
+            </MDBContainer>
+          </MDBMask>
+        </MDBView>
+      </div>
+    );
+  }
 }
