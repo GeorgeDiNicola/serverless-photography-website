f9ae3710101eaac49f4b094e9b014927f29a377b
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-05-23 14:11:59 -0400

    most of carousel from react-bootstrap lib

--- static/src/components/Home.jsx
+++ static/src/components/Home.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
-import Carousel from './Carousel.jsx';
-import "../css/carousel.css";
+import {Carousel} from 'react-bootstrap';
+import "../css/home.css"
+
+var listOfImages = [];
 
 export default class Home extends Component {
+
+	importAll(r) {
+        return r.keys().map(r);
+    }
+    componentWillMount() {
+        listOfImages = this.importAll(require.context('../carousel_images/', false, /\.(png|jpe?g|svg)$/));
+    }
 
 	render() {
 
 	  return (
-	    <div id="container">
-	      <Carousel />
-        </div>   
+	  	<div>
+	  	  <Carousel interval="3000">
+          {
+            listOfImages.map(
+              (image, index) =>
+                  <Carousel.Item>
+                    <img
+                      className="home-image"
+                      src={image}
+                      alt=""
+                    />
+                  </Carousel.Item>
+              )
+            }
+            </Carousel>
+          </div> 
 	  )
     }
 }
