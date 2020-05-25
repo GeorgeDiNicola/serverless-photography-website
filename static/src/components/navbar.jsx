8b68ae9ff307cb79cd2ee1ebe8e795d6522772af
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-05-25 14:22:03 -0400

    rolled back the navbar. issue with sizing background images because the navbar is not part of the background

--- static/src/components/navbar.jsx
+++ static/src/components/navbar.jsx
@@ -16,23 +16,27 @@ import {
 import '../css/navbar.css'
 
 // Stateless functional component
+
+
+// ISSUE WITH THE NAVBAR IS THAT THE IMAGES ARE PART OF THE BACKGROUND AND NAV IS NOT
+// WHICH MEANS THE IMAGE HEIGHT CANNOT ACCOUNT FOR THE SIZE OF THE NAVBAR
+// THEY WILL NEED TO BE ACTUAL IMAGES RATHER THAN BACKGROUND ONES MAYBE?
+
 export default function NavBar() {
 
 	return (
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top scrolling-navbar">
-        <div class="container-fluid">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <a class="navbar-brand" href="#">Billy Denham</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="collapse navbar-collapse" id="navbarNav">
           <div class="navbar-nav">
             <a class="nav-item nav-link active" href="#home">Home <span class="sr-only">(current)</span></a>
             <a class="nav-item nav-link" href="./Photography">Photography</a>
             <a class="nav-item nav-link" href="#videography">Videography</a>
             <a class="nav-item nav-link" href="#contact">Contact Me</a>
           </div>
-        </div>
         </div>
       </nav>
   );
