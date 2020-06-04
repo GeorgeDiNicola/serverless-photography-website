0da6a563a8e9033947555308e094a7f261470d4e
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-06-04 16:38:41 -0400

    began working with image gallery react component. a lot of styling issues so far

--- static/src/components/Photography.jsx
+++ static/src/components/Photography.jsx
@@ -1,32 +1,46 @@
 import React, { Component } from 'react';
+import ImageGallery from 'react-image-gallery';
 import Photo from './Photo';
 
 var listOfImages = [];
 
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+  ];
+
 export default class Photography extends Component {
 
-	importAll(r) {
-        return r.keys().map(r);
-    }
-    componentWillMount() {
-        listOfImages = this.importAll(require.context('../images/', false, /\.(png|jpe?g|svg)$/));
-    }
+  importAll(r) {
+    return r.keys().map(r);
+  }
+  componentWillMount() {
+    listOfImages = this.importAll(require.context('../images/', false, /\.(png|jpe?g|svg)$/));
+  }
 
-    render(){
+  render(){
 
-        return(
-          <div>
-          {
-            listOfImages.map(
-              (image, index) =>    
-                <Photo
-                  key={index} 
-                  src={image} 
-                  alt=""
-                />
-            )
-          }
-          </div>
-        )
+      return(
+        <div className="screen">
+          <ImageGallery 
+            items={images} 
+            showBullets={true}
+            showIndex={true}
+            showThumbnails={false}
+            lazyLoad={true}
+            showPlayButton={false}
+          />
+        </div>
+      )
     }
 }
