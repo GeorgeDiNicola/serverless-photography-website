79d728bb21062cdd250d8355628673836b24ee77
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-06-04 19:56:29 -0400

    finished drop-down menu options that disappears when the user clicks anywhere else on the page

--- static/src/components/Photography.jsx
+++ static/src/components/Photography.jsx
@@ -1,30 +1,80 @@
 import React, { Component } from 'react';
 import ImageGallery from 'react-image-gallery';
-import Photo from './Photo';
 import "react-image-gallery/styles/css/image-gallery.css";
+import "../css/photography.css";
 
 var listOfImages = [];
 var images = [];
+var container = React.createRef();
 
 export default class Photography extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      open: false,  // for the drop-down menu filter
+    };
   }
 
   importAll(r) {
     return r.keys().map(r);
   }
+
   componentWillMount() {
     // import the list
-    listOfImages = this.importAll(require.context('../images/nature/', false, /\.(png|jpe?g|svg)$/));
+    listOfImages = this.importAll(require.context('../images/events/', false, /\.(png|jpe?g|svg)$/));
     // create a objects from the imported listOfImages
     images = listOfImages.map(x => ({original: x, thumbnail: x}));
   }
 
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside);
+  }
+
+  handleButtonClick = () => {
+    this.setState(state => {
+      return {
+        open: !state.open,
+      };
+    });
+  };
+
+  handleClickOutside = event => {
+    if (container.current && !container.current.contains(event.target)) {
+      this.setState({
+        open: false,
+      });
+    }
+  };
+
   render(){
 
       return(
+        <React.Fragment>
+        <div className="container" ref={container}>
+          <button type="button" class="button" onClick={this.handleButtonClick}>
+             ☰
+          </button>
+            {this.state.open && (
+            <div class="dropdown">
+              <ul>
+                <li>All</li>
+                <li>Events</li>
+                <li>Nature</li>
+                <li>Portraits</li>
+                <li>Still Life</li>
+                <li>Street</li>
+                <li>Themed</li>
+                <li>Weddings</li>
+                <li>Wild Life</li>
+              </ul>
+            </div>
+            )}
+        </div>
         <div>
           <ImageGallery 
             items={images} 
@@ -32,9 +82,10 @@ export default class Photography extends Component {
             showIndex={true}
             showThumbnails={true}
             lazyLoad={true}
-            showPlayButton={false}
+            showPlayButton={true}
           />
         </div>
+        </React.Fragment>
       )
     }
 }
