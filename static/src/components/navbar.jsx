7ea4eba3d308aadc28d79481d7bc3664e98f7dbe
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-06-05 03:08:46 -0400

    saving point. added future ability to underline active web page

--- static/src/components/navbar.jsx
+++ static/src/components/navbar.jsx
@@ -2,28 +2,35 @@ import React from 'react';
 import '../css/navbar.css'
 
 // Stateless functional component
-/*
+export default function NavBar() {
+
+  var activeHomePage = 'nav-item active activated';
+  var activePhotoPage = 'nav-item active';
+  var activeVideoPage = 'nav-item active';
+  var activeContactPage = 'nav-item active';
+
+  /*
 
   USE ONCLICK METHOD TO CHANGE THE NAVIGATIONAL TEXT BASED ON THE SCREEN
 
-*/
+  navbar will maybe need to hold a state indicating which page it is on
 
-export default function NavBar() {
+  */
 
 	return (
     <nav class="navbar navbar-expand fixed-top navbar-light">
           <ul class="navbar-nav">
-              <li class="nav-item active">
+              <li class={activeHomePage}>
                 <a class="nav-link" href="./Home">Home</a>
               </li>
-              <li class="nav-item active">
+              <li class={activePhotoPage}>
                 <a class="nav-link" href="./Photography">Photos</a>
               </li>
               {/*<a href="./Home">Brand</a>*/}
-              <li class="nav-item active">
+              <li class={activeVideoPage}>
                 <a class="nav-link" href="./Videography">Videos</a>
               </li>
-              <li class="nav-item active">
+              <li class={activeContactPage}>
                 <a class="nav-link" href="./Contact">Contact</a>
               </li>
           </ul>
