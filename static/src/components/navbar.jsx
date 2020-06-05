5f522f57c9b016e732c645afe12282bbefef346c
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-06-05 04:55:23 -0400

    finished styling navbar rendering changes. also changed background on contact page to black

--- static/src/components/navbar.jsx
+++ static/src/components/navbar.jsx
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
 import '../css/navbar.css'
 
-// Stateless functional component
 export default class NavBar extends Component {
 
   constructor(props) {
     super(props);
 
     // class methods
-    this.activeHomePage = 'nav-item active';
-    this.activePhotoPage = 'nav-item active';
-    this.activeVideoPage = 'nav-item active';
-    this.activeContactPage = 'nav-item active';
     this.detectBrowserChange = this.detectBrowserChange.bind(this);
   }
   
@@ -19,24 +14,29 @@ export default class NavBar extends Component {
   detectBrowserChange() {
     var currentWindiw = window.location.href;
     if (currentWindiw.includes("Home")) {
-      this.activeHomePage = 'nav-item active activated';
-      this.activePhotoPage = this.activeVideoPage = this.activeContactPage ='nav-item active';
+      this.activeHomePage = 'nav-item activated-home';
+      this.activePhotoPage = this.activeVideoPage = this.activeContactPage ='nav-item';
+      this.currentPageId = 'home';
     }
     else if (currentWindiw.includes("Photography")) {
-      this.activePhotoPage = 'nav-item active activated';
-      this.activeHomePage = this.activeVideoPage = this.activeContactPage ='nav-item active';
+      this.activePhotoPage = 'nav-item activated-photo';
+      this.activeHomePage = this.activeVideoPage = this.activeContactPage ='nav-item';
+      this.currentPageId = 'photo';
     }
     else if (currentWindiw.includes("Videography")) {
-      this.activeVideoPage = 'nav-item active activated';
-      this.activeHomePage = this.activePhotoPage = this.activeContactPage ='nav-item active';
+      this.activeVideoPage = 'nav-item activated-video';
+      this.activeHomePage = this.activePhotoPage = this.activeContactPage ='nav-item';
+      this.currentPageId = 'video';
     }
     else if (currentWindiw.includes("Contact")) {
-      this.activeContactPage = 'nav-item active activated';
-      this.activeHomePage = this.activePhotoPage = this.activeVideoPage ='nav-item active';
+      this.activeContactPage = 'nav-item active activated-contact';
+      this.activeHomePage = this.activePhotoPage = this.activeVideoPage ='nav-item';
+      this.currentPageId = 'contact';
     }
     else {
-      this.activeHomePage = 'nav-item active activated';
-      this.activePhotoPage = this.activeVideoPage = this.activeContactPage ='nav-item active';
+      this.activeHomePage = 'nav-item activated-home';
+      this.activePhotoPage = this.activeVideoPage = this.activeContactPage ='nav-item';
+      this.currentPageId = 'home';
     }
   }
 
@@ -47,17 +47,17 @@ export default class NavBar extends Component {
       <nav class="navbar navbar-expand fixed-top navbar-light">
             <ul class="navbar-nav">
                 <li class={this.activeHomePage}>
-                  <a class="nav-link" href="./Home">Home</a>
+                  <a id={this.currentPageId} class="nav-link" href="./Home">Home</a>
                 </li>
                 <li class={this.activePhotoPage}>
-                  <a class="nav-link" href="./Photography">Photos</a>
+                  <a id={this.currentPageId} class="nav-link" href="./Photography">Photos</a>
                 </li>
                 {/*<a href="./Home">Brand</a>*/}
                 <li class={this.activeVideoPage}>
-                  <a class="nav-link" href="./Videography">Videos</a>
+                  <a id={this.currentPageId} class="nav-link" href="./Videography">Videos</a>
                 </li>
                 <li class={this.activeContactPage}>
-                  <a class="nav-link" href="./Contact">Contact</a>
+                  <a id={this.currentPageId} class="nav-link" href="./Contact">Contact</a>
                 </li>
             </ul>
       </nav>
