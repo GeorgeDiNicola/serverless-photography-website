8941b27f13918502c920daeaf2e20572a42fc407
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-06-05 04:33:58 -0400

    added functinality for detecting which page the browser is on

--- static/src/components/navbar.jsx
+++ static/src/components/navbar.jsx
@@ -1,39 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/navbar.css'
 
 // Stateless functional component
-export default function NavBar() {
+export default class NavBar extends Component {
 
-  var activeHomePage = 'nav-item active activated';
-  var activePhotoPage = 'nav-item active';
-  var activeVideoPage = 'nav-item active';
-  var activeContactPage = 'nav-item active';
+  constructor(props) {
+    super(props);
 
-  /*
+    // class methods
+    this.activeHomePage = 'nav-item active';
+    this.activePhotoPage = 'nav-item active';
+    this.activeVideoPage = 'nav-item active';
+    this.activeContactPage = 'nav-item active';
+    this.detectBrowserChange = this.detectBrowserChange.bind(this);
+  }
+  
 
-  USE ONCLICK METHOD TO CHANGE THE NAVIGATIONAL TEXT BASED ON THE SCREEN
+  detectBrowserChange() {
+    var currentWindiw = window.location.href;
+    if (currentWindiw.includes("Home")) {
+      this.activeHomePage = 'nav-item active activated';
+      this.activePhotoPage = this.activeVideoPage = this.activeContactPage ='nav-item active';
+    }
+    else if (currentWindiw.includes("Photography")) {
+      this.activePhotoPage = 'nav-item active activated';
+      this.activeHomePage = this.activeVideoPage = this.activeContactPage ='nav-item active';
+    }
+    else if (currentWindiw.includes("Videography")) {
+      this.activeVideoPage = 'nav-item active activated';
+      this.activeHomePage = this.activePhotoPage = this.activeContactPage ='nav-item active';
+    }
+    else if (currentWindiw.includes("Contact")) {
+      this.activeContactPage = 'nav-item active activated';
+      this.activeHomePage = this.activePhotoPage = this.activeVideoPage ='nav-item active';
+    }
+    else {
+      this.activeHomePage = 'nav-item active activated';
+      this.activePhotoPage = this.activeVideoPage = this.activeContactPage ='nav-item active';
+    }
+  }
 
-  navbar will maybe need to hold a state indicating which page it is on
-
-  */
-
-	return (
-    <nav class="navbar navbar-expand fixed-top navbar-light">
-          <ul class="navbar-nav">
-              <li class={activeHomePage}>
-                <a class="nav-link" href="./Home">Home</a>
-              </li>
-              <li class={activePhotoPage}>
-                <a class="nav-link" href="./Photography">Photos</a>
-              </li>
-              {/*<a href="./Home">Brand</a>*/}
-              <li class={activeVideoPage}>
-                <a class="nav-link" href="./Videography">Videos</a>
-              </li>
-              <li class={activeContactPage}>
-                <a class="nav-link" href="./Contact">Contact</a>
-              </li>
-          </ul>
-    </nav>
-  );
+  render() {
+    this.detectBrowserChange()
+	
+    return (
+      <nav class="navbar navbar-expand fixed-top navbar-light">
+            <ul class="navbar-nav">
+                <li class={this.activeHomePage}>
+                  <a class="nav-link" href="./Home">Home</a>
+                </li>
+                <li class={this.activePhotoPage}>
+                  <a class="nav-link" href="./Photography">Photos</a>
+                </li>
+                {/*<a href="./Home">Brand</a>*/}
+                <li class={this.activeVideoPage}>
+                  <a class="nav-link" href="./Videography">Videos</a>
+                </li>
+                <li class={this.activeContactPage}>
+                  <a class="nav-link" href="./Contact">Contact</a>
+                </li>
+            </ul>
+      </nav>
+    );
+  }
 }
