dcdeee0d754f3fb2946c50c350c8f0a35384acac
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-06-06 22:59:47 -0400

    added proper linkedIn hyperlink and added target=_blank to force it into a new tag

--- static/src/components/Contact.jsx
+++ static/src/components/Contact.jsx
@@ -13,7 +13,7 @@ export default class Contact extends Component {
             For general or work inquires please contact me at:<br/>
             <a href="mailto:williamdenham107@gmail.com">williamdenham107@gmail.com</a>
             <br/>
-            <a href="linkedin.com/in/william-denham-33724b180">Connect on LinkedIn</a>
+            <a href="https://linkedin.com/in/william-denham-33724b180" target="_blank">Connect on LinkedIn</a>
         </h6>
       </div>
     );
