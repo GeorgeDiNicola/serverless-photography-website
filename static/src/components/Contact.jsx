7f017c2b6be3aa950700c7f98e48fa11ec4a5983
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-06-06 21:50:34 -0400

    updated the contact page. Removed the MBD dependencies

--- static/src/components/Contact.jsx
+++ static/src/components/Contact.jsx
@@ -1,19 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
-import {
-  MDBCollapse,
-  MDBMask,
-  MDBRow,
-  MDBCol,
-  MDBIcon,
-  MDBBtn,
-  MDBView,
-  MDBContainer,
-  MDBCard,
-  MDBCardBody,
-  MDBInput,
-  MDBFormInline
-} from "mdbreact";
 import '../css/contactme.css';
 
 export default class Contact extends Component {
@@ -21,21 +6,15 @@ export default class Contact extends Component {
 	render() {
 
 	  return (
-	   <div id="classicformpage">
-        <MDBView>
-          <MDBMask className="d-flex justify-content-center align-items-center gradient">
-            <MDBContainer>
-              <h1>Billy Denham</h1>
-                <h6>
-                  Photographer based in Philadelphia, PA<br/>
-                  For general or work inquires please contact me at:<br/>
-                  <a href="mailto:williamdenham107@gmail.com">williamdenham107@gmail.com</a>
-                  <br/>
-                  <a href="linkedin.com/in/william-denham-33724b180">Connect on LinkedIn</a>
-                </h6>
-            </MDBContainer>
-          </MDBMask>
-        </MDBView>
+	    <div>
+        <h1>Billy Denham</h1>
+          <h6>
+            Photographer based in Philadelphia, PA<br/>
+            For general or work inquires please contact me at:<br/>
+            <a href="mailto:williamdenham107@gmail.com">williamdenham107@gmail.com</a>
+            <br/>
+            <a href="linkedin.com/in/william-denham-33724b180">Connect on LinkedIn</a>
+        </h6>
       </div>
     );
   }
