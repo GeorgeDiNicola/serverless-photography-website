6c5d5cb003f7f4e2b621260ef830a5df35ea2f88
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-06-07 20:44:42 -0400

    turned the dropdown button ID into a variable so I can make it black or white depending on which screen the navbar is on

--- static/src/components/navbar.jsx
+++ static/src/components/navbar.jsx
@@ -19,26 +19,31 @@ export default class NavBar extends Component {
       this.activeHomePage = 'nav-item activated-home';
       this.activePhotoPage = this.activeVideoPage = this.activeContactPage ='nav-item';
       this.currentPageId = 'home';
+      this.dropDownButtonId = 'dropdown-item-button-home'
     }
     else if (currentWindow.includes("Photography")) {
       this.activePhotoPage = 'nav-item activated-photo';
       this.activeHomePage = this.activeVideoPage = this.activeContactPage ='nav-item';
       this.currentPageId = 'photo';
+      this.dropDownButtonId = 'dropdown-item-button'
     }
     else if (currentWindow.includes("Videography")) {
       this.activeVideoPage = 'nav-item activated-video';
       this.activeHomePage = this.activePhotoPage = this.activeContactPage ='nav-item';
       this.currentPageId = 'video';
+      this.dropDownButtonId = 'dropdown-item-button'
     }
     else if (currentWindow.includes("Contact")) {
       this.activeContactPage = 'nav-item active activated-contact';
       this.activeHomePage = this.activePhotoPage = this.activeVideoPage ='nav-item';
       this.currentPageId = 'contact';
+      this.dropDownButtonId = 'dropdown-item-button'
     }
     else {
       this.activeHomePage = 'nav-item activated-home';
       this.activePhotoPage = this.activeVideoPage = this.activeContactPage ='nav-item';
       this.currentPageId = 'home';
+      this.dropDownButtonId = 'dropdown-item-button-home'
     }
   }
 
@@ -56,7 +61,7 @@ export default class NavBar extends Component {
                 </li>
                 */}
                 <li id={this.currentPageId} class={this.activePhotoPage}>
-                <DropdownButton id="dropdown-item-button" class="nav-link" title="Photos" onSelect={this.handleSelect}>
+                <DropdownButton id={this.dropDownButtonId} class="nav-link" title="Photos" onSelect={this.handleSelect}>
                   <Dropdown.Item href="./Photography#events">Events</Dropdown.Item>
                   <Dropdown.Item href="./Photography#nature">Nature</Dropdown.Item>
                   <Dropdown.Item href="./Photography#portraits">Portraits</Dropdown.Item>
