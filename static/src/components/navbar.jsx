677d3b15c3e10e8d669dd15b5052c90a6850b4c0
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-06-08 17:26:13 -0400

    changed the navbar for the video page to stick, giving it an in-place effect

--- static/src/components/navbar.jsx
+++ static/src/components/navbar.jsx
@@ -18,22 +18,27 @@ export default class NavBar extends Component {
     if (currentWindow.includes("Home")) {
       this.activeHomePage = 'nav-item activated-home';
       this.activePhotoPage = this.activeVideoPage = this.activeContactPage ='nav-item';
+      this.parentNavClass = "navbar navbar-expand"
     }
     else if (currentWindow.includes("Photography")) {
       this.activePhotoPage = 'nav-item activated-photo';
       this.activeHomePage = this.activeVideoPage = this.activeContactPage ='nav-item';
+      this.parentNavClass = "navbar navbar-expand"
     }
     else if (currentWindow.includes("Videography")) {
       this.activeVideoPage = 'nav-item activated-video';
       this.activeHomePage = this.activePhotoPage = this.activeContactPage ='nav-item';
+      this.parentNavClass = "navbar fixed-top navbar-expand"
     }
     else if (currentWindow.includes("Contact")) {
       this.activeContactPage = 'nav-item active activated-contact';
       this.activeHomePage = this.activePhotoPage = this.activeVideoPage ='nav-item';
+      this.parentNavClass = "navbar navbar-expand"
     }
     else {
       this.activeHomePage = 'nav-item activated-home';
       this.activePhotoPage = this.activeVideoPage = this.activeContactPage ='nav-item';
+      this.parentNavClass = "navbar navbar-expand"
     }
   }
 
@@ -41,7 +46,7 @@ export default class NavBar extends Component {
     this.detectBrowserChange()
 	
     return (
-      <nav class="navbar navbar-expand">
+      <nav class={this.parentNavClass}>
             <ul class="navbar-nav">
                 <li class={this.activeHomePage}>
                   <a class="nav-link" href="./Home">Home</a>
