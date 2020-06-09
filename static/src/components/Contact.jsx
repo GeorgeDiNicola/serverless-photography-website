6f7ffe96b11e09fbed48579a0d8deef64c61cfa9
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-06-09 01:06:02 -0400

    added social media buttons to the contact page

--- static/src/components/Contact.jsx
+++ static/src/components/Contact.jsx
@@ -9,43 +9,29 @@ export default class Contact extends Component {
 
 	  return (
 
-      <div className="parentContainer">
-      
-
-      <div className="w3-animate-left left inline">
+      <React.Fragment>
+      <div className="w3-animate-left parentContainer">
         <h1>Billy Denham</h1>
           <h6>
             Photographer based in Philadelphia, PA<br/>
             For general or work inquires please contact me at<br/>
             <a href="mailto:williamdenham107@gmail.com">williamdenham107@gmail.com</a>
             <br/>
+              <a href="https://www.linkedin.com/in/william-denham-33724b180/" class="fa fa-linkedin" target="_blank"></a>
+              <a href="https://www.instagram.com/billy.denham/?hl=en" class="fa fa-instagram" target="_blank"></a>
             <br/>
-            <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="georgedinicola">
-              <a class="LI-simple-link" href='https://www.linkedin.com/in/georgedinicola?trk=profile-badge'>
-                George DiNicola
-              </a>
-            </div>
         </h6>
       </div>
-
-      <div className="w3-animate-right right inline">
-        <h1>George DiNicola</h1>
+      <div className="w3-animate-right">
+        <footer>
           <h6>
-            Web Development & Design<br/>
-            Full Stack Engineer<br/>
-            <a href="mailto:george.j.dinicola@gmail.com">george.j.dinicola@gmail.com</a>
-            <br/>
-            <br/>
-            <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="georgedinicola">
-              <a class="LI-simple-link" href='https://www.linkedin.com/in/georgedinicola?trk=profile-badge'>
-                George DiNicola
-              </a>
-            </div>
-        </h6>
+          <a href="https://github.com/GeorgeDiNicola" class="fa fa-github" target="_blank"></a>
+          <br/>
+          Web Design & Development: George DiNicola 
+          </h6> 
+        </footer>
       </div>
-
-
-      </div>
+      </React.Fragment>
     );
   }
 }
