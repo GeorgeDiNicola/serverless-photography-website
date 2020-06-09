354c16739c16d167d0347ea1d7f3d6225255e0cf
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-06-08 22:44:52 -0400

    added the better quality image and removed the white background

--- static/src/components/Contact.jsx
+++ static/src/components/Contact.jsx
@@ -19,26 +19,28 @@ export default class Contact extends Component {
             For general or work inquires please contact me at<br/>
             <a href="mailto:williamdenham107@gmail.com">williamdenham107@gmail.com</a>
             <br/>
-            <input type="image" src={require('../images/IG_Glyph_Fill.jpg')} />
             <br/>
+            <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="georgedinicola">
+              <a class="LI-simple-link" href='https://www.linkedin.com/in/georgedinicola?trk=profile-badge'>
+                George DiNicola
+              </a>
+            </div>
         </h6>
       </div>
 
       <div className="w3-animate-right right inline">
         <h1>George DiNicola</h1>
           <h6>
-            Photographer based in Philadelphia, PA<br/>
-            For general or work inquires please contact me at<br/>
+            Web Development & Design<br/>
+            Full Stack Engineer<br/>
             <a href="mailto:george.j.dinicola@gmail.com">george.j.dinicola@gmail.com</a>
             <br/>
             <br/>
-            {/*}
             <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="georgedinicola">
               <a class="LI-simple-link" href='https://www.linkedin.com/in/georgedinicola?trk=profile-badge'>
                 George DiNicola
               </a>
             </div>
-          */}
         </h6>
       </div>
 
