dd15e5fc35f5695aee91fc6d93cd2db1c3883da8
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-06-11 20:00:16 -0400

    added contact form. sizing is not working properly. the width is not inheriting the true width from the page body

--- static/src/components/Contact.jsx
+++ static/src/components/Contact.jsx
@@ -13,21 +13,36 @@ export default class Contact extends Component {
           <h6>
             Photographer based in Philadelphia, PA<br/>
             For general or work inquires please contact me at<br/>
-            <a href="mailto:williamdenham107@gmail.com">williamdenham107@gmail.com</a>
-            <br/>
-              <a href="https://www.linkedin.com/in/william-denham-33724b180/" class="fa fa-linkedin" target="_blank"></a>
-              <a href="https://www.instagram.com/billy.denham/?hl=en" class="fa fa-instagram" target="_blank"></a>
-            <br/>
-        </h6>
+          <a href="mailto:williamdenham107@gmail.com">williamdenham107@gmail.com</a>
+          <br/>
+          <a href="https://www.linkedin.com/in/william-denham-33724b180/" class="fa fa-linkedin" target="_blank"></a>
+          <a href="https://www.instagram.com/billy.denham/?hl=en" class="fa fa-instagram" target="_blank"></a>
+          <br/>
+          </h6>
       </div>
+     
+      <div class="container">
+        <form>
+          {/*<label for="fname">First Name</label>*/}
+          <input type="text" id="fname" name="firstname" placeholder="Your first name.."></input>
+          {/*<label for="lname">Last Name</label>*/}
+          <input type="text" id="lname" name="lastname" placeholder="Your last name.."></input>
+          <br/>
+          {/*<label for="message">Message</label>*/}
+          <textarea id="message" name="message" placeholder="Write a message.."></textarea>
+          <br/>
+          <input type="submit" value="Submit"></input>
+        </form>
+      </div>
+
       <div className="w3-animate-right">
-        <footer>
+        <div className="footer">
           <h6>
           <a href="https://github.com/GeorgeDiNicola" class="fa fa-github" target="_blank"></a>
           <br/>
           Web Design & Development: George DiNicola 
           </h6> 
-        </footer>
+        </div>
       </div>
       </React.Fragment>
     );
