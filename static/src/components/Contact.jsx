601dff600cf83d989a5f5e9a194be00e148397b6
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-06-11 21:09:02 -0400

    got footer to not overlap content when scrolling, but it won't stay at bottom

--- static/src/components/Contact.jsx
+++ static/src/components/Contact.jsx
@@ -8,18 +8,21 @@ export default class Contact extends Component {
 	  return (
 
       <React.Fragment>
-      <div className="w3-animate-left">
-        <h1>Billy Denham</h1>
-          <h6>
-            Photographer based in Philadelphia, PA<br/>
-            For general or work inquires please contact me at<br/>
-          <a href="mailto:williamdenham107@gmail.com">williamdenham107@gmail.com</a>
-          <br/>
-          <a href="https://www.linkedin.com/in/william-denham-33724b180/" class="fa fa-linkedin" target="_blank"></a>
-          <a href="https://www.instagram.com/billy.denham/?hl=en" class="fa fa-instagram" target="_blank"></a>
-          <br/>
-          </h6>
-      </div>
+      <div id="page-container">
+      
+      <div id="content-wrap">
+        <div className="w3-animate-left">
+          <h1>Billy Denham</h1>
+            <h6>
+              Photographer based in Philadelphia, PA<br/>
+              For general or work inquires please contact me at<br/>
+            <a href="mailto:williamdenham107@gmail.com">williamdenham107@gmail.com</a>
+            <br/>
+            <a href="https://www.linkedin.com/in/william-denham-33724b180/" class="fa fa-linkedin" target="_blank"></a>
+            <a href="https://www.instagram.com/billy.denham/?hl=en" class="fa fa-instagram" target="_blank"></a>
+            <br/>
+            </h6>
+        </div>
      
       <div className="form-container">
         <form>
@@ -34,16 +37,20 @@ export default class Contact extends Component {
           <input type="submit" value="Submit"></input>
         </form>
       </div>
-
-      <div className="w3-animate-right">
-        <div className="footer">
-          <h6>
-          <a href="https://github.com/GeorgeDiNicola" class="fa fa-github" target="_blank"></a>
-          <br/>
-          Web Design & Development: George DiNicola 
-          </h6> 
-        </div>
+      
       </div>
+      
+        <footer>
+          <div className="w3-animate-right">
+            <h6>
+            <a href="https://github.com/GeorgeDiNicola" class="fa fa-github" target="_blank"></a>
+            <br/>
+              Web Design & Development: George DiNicola 
+            </h6> 
+           </div>
+        </footer>
+        
+        </div>
       </React.Fragment>
     );
   }
