f1925f143272bb93ceff8be02d6f4710ad76ef2f
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-06-11 21:58:47 -0400

    form is now set up properly. content is distributed well. still need to style the form text and spacing between boxes

--- static/src/components/Contact.jsx
+++ static/src/components/Contact.jsx
@@ -10,12 +10,11 @@ export default class Contact extends Component {
       <React.Fragment>
       <div id="page-container">
       
-
+      <div className="about-content">
         <div className="w3-animate-left">
           <h1>Billy Denham</h1>
             <h6>
               Photographer based in Philadelphia, PA<br/>
-              For general or work inquires please contact me at<br/>
             <a href="mailto:williamdenham107@gmail.com">williamdenham107@gmail.com</a>
             <br/>
             <a href="https://www.linkedin.com/in/william-denham-33724b180/" class="fa fa-linkedin" target="_blank"></a>
@@ -23,9 +22,10 @@ export default class Contact extends Component {
             <br/>
             </h6>
         </div>
-     
+     </div>
       <div className="form-container">
         <form>
+          <h6>For general or work inquires please fill out the form below</h6><br/>
           {/*<label for="fname">First Name</label>*/}
           <input type="text" id="fname" name="firstname" placeholder="Your first name.."></input>
           {/*<label for="lname">Last Name</label>*/}
@@ -39,11 +39,11 @@ export default class Contact extends Component {
       </div>
     <div className="footer">
           <div className="w3-animate-right">
-            <h6>
-            <a href="https://github.com/GeorgeDiNicola" class="fa fa-github" target="_blank"></a>
-            <br/>
-              Web Design & Development: George DiNicola 
-            </h6> 
+            <span className="footer-text">
+              <a href="https://github.com/GeorgeDiNicola" class="fa fa-github" target="_blank"></a>
+              <br/>
+                Web Design & Development: George DiNicola 
+            </span> 
            </div>
     </div>
         </div>
