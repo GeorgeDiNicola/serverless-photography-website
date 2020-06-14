f99383bec804e4931340630026c08f83991bbc84
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-06-14 15:06:52 -0400

    changed videography and home to stateless functional components

--- static/src/components/Home.jsx
+++ static/src/components/Home.jsx
@@ -3,11 +3,7 @@ import {Carousel} from 'react-bootstrap';
 import "../css/home.css";
 import Videography from './Videography';  // without this the photography page will not render properly
 
-import Loader from 'react-loader-spinner';
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import "../css/loaders.css";
-
-var listOfClasses = [
+var listOfImages = [
 					"logo-black",
           "home-image1",
 					"home-image2",
@@ -20,47 +16,19 @@ var listOfClasses = [
           "logo-black",
           "home-image7",
           "home-image8"
-		];
+];
 
-export default class Home extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      component: null,
-    };
-  }
-
-  componentDidMount() {
-    // set the component state asynchronously
-    setTimeout(() => {
-      this.setState({
-        component: <Carousel interval="2000" pause={false}>
-        {
-          listOfClasses.map(
-            (c, index) =>
-              <Carousel.Item>
-                <img
-                  className={c}
-                  //src={image}
-                  alt=""
-                />
-              </Carousel.Item>
-          )
-         }
-        </Carousel>
-      });
-    });  // ,5000); to show the spinner
-  }
-
-	render() {
-
-    return (
-        this.state.component || (
-          <div className="loader">
-            <Loader type="BallTriangle" color="#00BFFF" height={80} width={80} />
-          </div>
-        )
-     )
-  }
-}
+const Home = () => (
+  <Carousel interval="2000" pause={false}>
+    {listOfImages.map((c, index) =>
+      <Carousel.Item>
+        <img
+          className={c}
+          //src={image}
+          alt=""
+         />
+      </Carousel.Item>
+    )}
+  </Carousel>
+)
+export default Home;
--- static/src/components/Videography.jsx
+++ static/src/components/Videography.jsx
@@ -2,56 +2,27 @@ import React, { Component } from 'react';
 import ReactPlayer from "react-player";
 import "react-bootstrap";
 import "../css/video.css";
-import "../css/loaders.css";
 
-import Loader from 'react-loader-spinner';
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import "../css/loaders.css";
+var listOfYoutubeLinks = [
+  "https://www.youtube.com/watch?v=lABN5Gld4FQ", 
+  "https://www.youtube.com/watch?v=DRaWc9GhYRk"
+];
 
-export default class Videography extends Component {
-
-	constructor(props) {
-		super(props);
-		this.state = {
-			video_urls: ["https://www.youtube.com/watch?v=lABN5Gld4FQ", "https://www.youtube.com/watch?v=DRaWc9GhYRk"],
-			component: null,
-		}
-	}
-
-	componentDidMount() {
-	  // set the component state asynchronously
-	  setTimeout(() => {
-	    this.setState({
-	      component: 
-	        <React.Fragment>
-	        <div className="parentContainer">
-		    <div class="container">
-		      {this.state.video_urls.map(url_text => 
-		        <div className="player-wrapper">
-		          <ReactPlayer
-		            className='react-player'
-		             url={url_text}
-		             controls={true}
-		             width='100%'
-	                 height='100%'
-		          />
-		        </div>
-		      )}
-		    </div>
-	      </div>
-          </React.Fragment>
-          });
-	    }, 2000);  // ,5000); to show the spinner
-	  }
-
-	render() {
-		return (
-		  this.state.component || (
-		  <div className="loader">
-            <Loader type="BallTriangle" color="#00BFFF" height={80} width={80} />
-          </div>
-          )
-		)
-
-	}
-}
+const Videography = () => (
+  <div className="parentContainer">
+    <div class="container">
+	  {listOfYoutubeLinks.map(url_text => 
+	    <div className="player-wrapper">
+		  <ReactPlayer
+		    className='react-player'
+		    url={url_text}
+		    controls={true}
+		    width='100%'
+	        height='100%'
+		  />
+		</div>
+	  )}
+	</div>
+  </div>
+)
+export default Videography;
