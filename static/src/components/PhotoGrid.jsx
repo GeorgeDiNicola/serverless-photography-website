a07c556f6659d2b6cd82a6a306be73d46bafc51f
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-06-18 00:00:56 -0400

    found a way to get the dimensions from the photos

--- static/src/components/PhotoGrid.jsx
+++ static/src/components/PhotoGrid.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Gallery from 'react-grid-gallery';
+import Photo from './Photo.jsx';
 import "../css/photogrid.css";
 
 
 var listOfImages = [];
 var images = [];
 
+
 export default class PhotoGrid extends Component {
+
+  getMeta(url){   
+    var img = new Image();
+    img.onload = function(){
+       console.log(this.width+' '+ this.height);
+    };
+    img.src = url;
+   }
 
   importAll(r) {
     return r.keys().map(r);
@@ -15,6 +25,12 @@ export default class PhotoGrid extends Component {
 
   getPhotos() {
   	listOfImages = this.importAll(require.context('../images/', true, /\.(png|jpe?g|svg)$/));
+    var index;
+    for (index = 0; index < listOfImages.length; ++index) {
+       this.getMeta(listOfImages[index]);
+       console.log(listOfImages[index]);
+    };
+
     // create an object from the imported listOfImages
     images = listOfImages.map(x => ({src: x, thumbnail: x, width: 4, height: 3}));
     //this.shuffleList(images);
@@ -30,17 +46,21 @@ export default class PhotoGrid extends Component {
 
 	this.getPhotos();
 
+	{images.map(src => 
+		  	<Photo 
+		  	  counter={src}
+		  	/>
+		  )}
+
     return(
       <div className="gridContainer">
       <Gallery 
         images={images}
         showLightboxThumbnails={true}
         backdropClosesModal={true}
-        margin={4}
-        direction={"column"}
+        margin={7}
       />
       </div>
     );
   }
-
- }
+}
