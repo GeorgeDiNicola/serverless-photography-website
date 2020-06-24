e1c2cb5d1ee92d4402ffae830e95b7ff152ccf69
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-06-24 11:12:31 -0400

    removed the loader

--- static/src/App.jsx
+++ static/src/App.jsx
@@ -2,39 +2,9 @@ import React, { Component } from 'react';
 import AppRouter from './components/AppRouter.jsx';
 import './App.css';
 
-import Loader from 'react-loader-spinner';
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import "./css/loaders.css";
 
-import { createBrowserHistory } from 'history';
+const App = () => (
 
-
-export default class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      component: null,
-    };
-  }
-
-  componentDidMount() {
-   // set the component state asynchronously
-    setTimeout(() => {
-      this.setState({ component:
-        <AppRouter />
-      });
-    }, 1000);  // ,5000); to show the spinner
-  }
-
-  render() {
-
-    return (
-      this.state.component || (
-        <div className="loader">
-          <Loader type="BallTriangle" color="#00BFFF" height={80} width={80} />
-        </div>
-      )
-    );
-  }
-}
+  <AppRouter />
+)
+export default App;
