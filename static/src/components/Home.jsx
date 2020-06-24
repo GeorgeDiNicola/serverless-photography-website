51529e5bd12516b6684dadb8d3e4e03f1ea900a0
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-06-23 23:56:53 -0400

    altered home page to resize on mobile screens and swapped out a photo that did not have enough height for proper rendering

--- static/src/components/Home.jsx
+++ static/src/components/Home.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import {Carousel} from 'react-bootstrap';
 import "../css/home.css";
-import Videography from './Videography';  // without this the photography page will not render properly
 
-var listOfImages = [
+const listOfImages = [
           require("../brand/branded_black.jpg"),
           require("../images/wild_life/3_2905_1939.jpg"),
-          require("../images/nature/25_2048_904.jpg"),
+          require("../images/nature/9_2048_1367.jpg"),
           require("../images/wild_life/2_4777_3164.jpg"),
           require("../images/street/2_2048_1367.jpg"),
           require("../images/nature/3_4257_2820.jpg"),
