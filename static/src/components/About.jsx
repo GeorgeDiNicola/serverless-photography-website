3966ed48e6c9ef5a7f2f3b2023a847a77b197df4
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-07-05 13:26:25 -0400

    updated to a cutout PNG file and edited the text content

--- static/src/components/About.jsx
+++ static/src/components/About.jsx
@@ -7,12 +7,12 @@ const About = () => (
     <div className="left-aligned-image">
       <img 
         className="portrait-image"
-        src={require("../brand/billy.jpg")}
+        src={require("../brand/billy.png")}
       ></img>
       <div className="about-text">Billy Denham
       </div>
     <div className="right-aligned-text">
-      is an experienced content creator with a demonstrated history of working in the marketing and advertising industry. Skilled in Photography, Film Production, Lightroom, After Effects, and Adobe Premiere Pro. Strong media and communication professional with a Bachelor of Arts - BA focused in Directing and Film/Video Production from Temple University. 
+      is an experienced content creator in the marketing and advertising industry. Skilled in photography, film production, as well as Adobe Lightroom, After Effects, and Premiere Pro. Strong media and communication professional with a Bachelor of Arts - BA focused in Directing and Film/Video Production from Temple University. 
     </div>
     </div>
   </div>
