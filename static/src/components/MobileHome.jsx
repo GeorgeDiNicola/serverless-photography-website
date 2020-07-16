d2801113a3d42df8dc363be599127f9310a61b43
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2020-07-15 23:08:38 -0400

    switched mobile homepage back to portrait images

--- static/src/components/MobileHome.jsx
+++ static/src/components/MobileHome.jsx
@@ -6,23 +6,21 @@ import MobileContact from './MobileContact.jsx';
 import "../css/home.css";
 
 const homeImages = [
-          require("../brand/branded_black.jpg"),
-          require("../images/wild_life/3_2905_1939.jpg"),
-          require("../images/nature/9_2048_1367.jpg"),
-          require("../images/wild_life/2_4777_3164.jpg"),
-          require("../images/street/2_2048_1367.jpg"),
-          require("../brand/branded_black.jpg"),
-          require("../images/nature/3_4257_2820.jpg"),
-          require("../images/nature/14_4185_2772.jpg"),
-          require("../images/wild_life/1_3872_2592.jpg"),
-          require("../images/street/6_2048_1367.jpg"),
+          require("../brand/branded_black_portrait.jpg"),
+          require("../images/street/14_1367_2048.jpg"),
+          require("../brand/branded_black_portrait.jpg"),
+          require("../images/nature/10_1367_2048.jpg"),
+          require("../brand/branded_black_portrait.jpg"),
+          require("../images/events/5_1367_2048.jpg"),
+          require("../brand/branded_black_portrait.jpg"),
+          require("../images/still_life/10_2686_4055.jpg"),
 ];
 
 const MobileHome = () => (
   
   <React.Fragment>
   <Carousel
-    interval="2500" 
+    interval="4000" 
     pause={false} 
     controls={false}
     fade={false}
